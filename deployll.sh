5dc626eeeae29214768f5790b3ea288f668751a3
Louis <manson.louis@gmail.com>
2019-01-24 14:42:20 -0800

    set examples names

--- deployll.sh
+++ deployll.sh
@@ -2119,39 +2119,40 @@ if [[ $LOCAL_INSTALL == true ]] && [[ $UPDATE_MODE == false ]]; then
             if [[ $n == "y" ]]; then
                 while true; do
                     output "please enter the organisation name"
-                    read e
+                    read -r -s -n 1 n
                     output_log "user entered '${e}'"
-                    if [[ $e == "" ]]; then
-                        e="Example"
-                        INSTALL_ORG=$e
+                    if [[ $n == "" ]]; then
+                        n="Example"
+                        INSTALL_ORG=$n
                         break
                     fi
                 done
                 while true; do
                     output "please enter the email address for the administrator account"
-                        read e
+                        read -r -s -n 1 n
                     output_log "user entered '${e}'"
-                    if [[ $e == "" ]]; then
-                        e="admin@example.org"
-                        INSTALL_EMAIL=$e
+                    if [[ $n == "" ]]; then
+                        n="admin@example.org"
+                        INSTALL_EMAIL=$n
                         break
                     fi
                 done
                 while true; do
                     while true; do
                         output "please enter the password for the administrator account"
-                        read -r -s e
-                        if [[ $e == "" ]]; then
-                            e="Example1."
-                            INSTALL_PASSWD=$e
+                        read -r -s -n 1 n
+                        if [[ $n == "" ]]; then
+                            n="Example1."
+                            INSTALL_PASSWD=$n
                             break
                         fi
                     done
                     while true; do
                         output "please confirm the password for the administrator account"
-                        read -r -s e
-                        if [[ $e != "" ]]; then
-                            if [[ $e == $INSTALL_PASSWD ]]; then
+                        read -r -s -n 1 n
+                        if [[ $n == "" ]]; then
+                            n="Example1."
+                            if [[ $n == $INSTALL_PASSWD ]]; then
                                 output_log "user entered passwords that matched - not writing them to the log for obvious reasons"
                                 break 2
                             else
